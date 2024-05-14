1c2054315979f98d2831e23cfcaabad5aaeea964
Suyash Bhiste <suyashbhiste0@gmail.com>
2024-05-14 23:17:23 +0530

    Created /submit and /result endpoint, added logic and added secrets

---
+++ Controllers/SubmissionController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineJudgeBackend.Models;
+using OnlineJudgeBackend.Services;
+
+namespace OnlineJudgeBackend.Controllers;
+
+public static class SubmissionController
+{
+    public static IServiceCollection RegisterSubmissionController(this IServiceCollection services)
+    {
+        services.AddSingleton<ISubmission, Submission>();
+        services.AddHttpClient<Submission>();
+        return services;
+    }
+
+    public static IEndpointRouteBuilder MapSubmissionEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapPost("/submit", 
+            async ([FromBody] SubmitRequest request, ISubmission submission) => 
+            await submission.SubmitCodeAsync(request));
+
+        endpoints.MapGet("/result", 
+            async (string id, ISubmission submission) =>
+                await submission.GetResultAsync(id));
+
+        return endpoints;
+    }
+}
